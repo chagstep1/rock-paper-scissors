2b67f923ff87a88c129ab33450b3658166a6836f
chagstep1 <yandex@chagstep1.ru>
2021-12-28 16:02:55 +0300

    Implement HTML ui

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const ROCK = "rock"
 const PAPER = "paper"
 const SCISSORS = "scissors"
 const gesturesTotal = 3
-const roundsTotal = 5
+const victoryScore = 5
+// should be enum when I figure out how to do it in JavaScript
+const E_WON = 0
+const E_LOST = 1
+const E_TIE = 2
+const E_ERROR = -1
+
+let playerScore = 0;
+let computerScore = 0;
+let gameIsDone = false;
 
 function computerPlay() {
 	switch (Math.floor(Math.random() * gesturesTotal)) {
@@ -29,36 +38,74 @@ function ourGestureBeats(ourGesture, theirGesture)
 }
 
 function playRound(playerSelection, computerSelection) {
-	let lowerPlayerSelection = playerSelection.toLowerCase();
-	let s_result = "Round is being played. Player: " + lowerPlayerSelection + ". Computer: " + computerSelection + ".\n";
-	if (lowerPlayerSelection === computerSelection)
+	if (playerSelection === computerSelection)
 	{
-		s_result += "Tied!";
+		return E_TIE;
 	}
-	else if (ourGestureBeats(lowerPlayerSelection, computerSelection))
+	else if (ourGestureBeats(playerSelection, computerSelection))
 	{
-		s_result += "You've won!";
+		return E_WON;
 	}
-	else if (ourGestureBeats(computerSelection, lowerPlayerSelection))
+	else if (ourGestureBeats(computerSelection, playerSelection))
 	{
-		s_result += "You've lost!";
+		return E_LOST;
 	}
 	else
 	{
-		s_result = "Bad input!";
+		return E_ERROR;
 	}
-	return s_result;
 }
 
-function game() {
-	alert(`Total of ${roundsTotal} rounds will be played!`);
-	for (let i = 0; i < roundsTotal; ++i)
+
+const resultDiv = document.querySelector('#result-div');
+const playerScoreSpan = document.querySelector('#player-score');
+const computerScoreSpan = document.querySelector('#computer-score');
+
+function playGameAndShow(playerSelection) {
+	if (gameIsDone) return;
+	
+	const computerSelection = computerPlay();
+	let sResult = "Round is being played. Player: " + playerSelection + ". Computer: " + computerSelection + ".<br>";
+	const eRoundResult = playRound(playerSelection, computerSelection);
+	switch (eRoundResult) {
+		case E_WON:
+			sResult += "You've won!";
+			++playerScore;
+			playerScoreSpan.textContent = playerScore;
+			break;
+		case E_LOST:
+			sResult += "You've lost!";
+			++computerScore;
+			computerScoreSpan.textContent = computerScore;
+			break;
+		case E_TIE:
+			sResult += "Tied!";
+			break;
+		default:
+			sResult = "Error!";
+			break;
+	}
+	
+	if (playerScore >= victoryScore)
 	{
-		alert("Game round #" + (i+1).toString());
-		let playerSelection = prompt(`Input either ${ROCK}, ${PAPER} or ${SCISSORS}`)
-		if (typeof(playerSelection) === "null") break;
-		alert(playRound(playerSelection, computerPlay()));
+		gameIsDone = true;
+		sResult += "<br><br>Player has reached the victory score!<br>Game over."
 	}
+	else if (computerScore >= victoryScore)
+	{
+		gameIsDone = true;
+		sResult += "<br><br>Computer has reached the victory score!<br>Game over."
+	}
+	
+	resultDiv.innerHTML = sResult;
 }
 
-game();
+
+const buttonRock = document.querySelector('#rock');
+buttonRock.addEventListener('click', () => playGameAndShow(`${ROCK}`));
+
+const buttonPaper = document.querySelector('#paper');
+buttonPaper.addEventListener('click', () => playGameAndShow(`${PAPER}`));
+
+const buttonScissors = document.querySelector('#scissors');
+buttonScissors.addEventListener('click', () => playGameAndShow(`${SCISSORS}`));
